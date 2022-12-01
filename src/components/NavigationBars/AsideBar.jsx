fd3321e864e455580bc6da2091aa86276ebe3ea6
Lambert Bastien <bastien.lambert@univ-grenoble-alpes.fr>
2022-12-02 00:45:25 +0100

    Asidebar Image loading

--- src/components/NavigationBars/AsideBar.jsx
+++ src/components/NavigationBars/AsideBar.jsx
@@ -1,14 +1,31 @@
 import React from 'react';
 import "./AsideBar.css";
 
+function createImage(){
+    
+}
 
-function loadListe() {
-    return (<li><img src="./assets/bubble-sprites.png" width="100"></img></li>)
+
+function loadImages(){
+    const ab = document.querySelector(".AsideBar");
+    for(var i=0;i<5;i++)
+    {
+        var div = new HTMLDivElement();
+        var img = new HTMLImageElement();
+        img.src = "./assets/bubble-sprites.png";
+        div.appendChild(img);
+        ab.append(img);
+    }
+
+}
+function loadDescription(){
+
 }
 
 const AsideBar = () => {
     return (
       <div className="AsideBar">
+        {loadImages()}
       </div>
     );
 }
