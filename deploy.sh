cbba6efd832ce25355c3d24ba00c74cf9b7eed79
Lucas <lucas.mathieu1@etu.univ-grenoble-alpes.fr>
2022-12-01 23:55:25 +0100

    Fix CD (#3)

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,6 @@ cd ISToire_de_Q
 git pull
 npm i
 docker build . -t dortoir/istoire_de_q
-docker run -p 7456:3000 -d dortoir/istoire_de_q
+docker stop istoire_de_q
+docker rm istoire_de_q
+docker run -p 7456:3000 -d dortoir/istoire_de_q --name=istoire_de_q
