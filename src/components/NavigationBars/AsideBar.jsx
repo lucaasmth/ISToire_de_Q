aab940667ea373c9ebe74bb62635a02c464abfff
Lambert Bastien <bastien.lambert@univ-grenoble-alpes.fr>
2022-12-01 23:53:09 +0100

    Added AsideBar

---
+++ src/components/NavigationBars/AsideBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./AsideBar.css";
+
+
+function loadListe() {
+    return (<li><img src="./assets/bubble-sprites.png" width="100"></img></li>)
+}
+
+const AsideBar = () => {
+    return (
+      <div className="AsideBar">
+      </div>
+    );
+}
+export default AsideBar;
