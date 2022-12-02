3b79f5f27324d1acc58347b937babbac65df4d80
Yann MLEKO <yann.mlekoy@gmail.com>
2022-12-02 03:08:06 +0100

    Updated crossword, added css Updated crossword colors and added a score Also added a css

--- src/components/crosswords/Crosswords.jsx
+++ src/components/crosswords/Crosswords.jsx
@@ -1,80 +1,80 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 import Crossword, { ThemeProvider } from '@jaredreisinger/react-crossword';
+import "./anim.css";
 
 const data = {
     
         across: {
-          1: {
-            clue: 'Groupe de virus',
-            answer: 'PAPILLOMAVIRUS' ,
-            row: 10,
-            col: 0,
-          },
-          2: {
-            clue: 'Monter au 7ème ciel',
-            answer: 'ORGASME',
-            row: 8,
-            col: 2,
+            1: {
+                clue: 'Groupe de virus',
+                answer: 'PAPILLOMAVIRUS' ,
+                row: 10,
+                col: 0,
+            },
+            2: {
+                clue: 'Monter au 7ème ciel',
+                answer: 'ORGASME',
+                row: 8,
+                col: 2,
 
-          },
-          3: {
-            clue:'Virus responsable du sida',
-            answer: 'VIH',
-            row: 12,
-            col:7,
-          },
-          4: {
-            clue:'Mesures pour contrer des risques',
-            answer:'PREVENTION',
-            row: 14,
-            col: 6,},
-            5: {
-                clue:'Couvre chef',
-                answer:'PRESERVATIF',
+            },
+            3: {
+                clue:'Virus responsable du sida',
+                answer: 'VIH',
+                row: 12,
+                col:7,
+            },
+            4: {
+                clue:'Mesures pour contrer des risques',
+                answer:'PREVENTION',
+                row: 14,
+                col: 6,},
+                5: {
+                    clue:'Couvre chef',
+                    answer:'PRESERVATIF',
+                    row: 16,
+                    col: 13,
+                },
+                6: {
+                    clue:'Virus très contagieux formant des cloques/plaies',
+                    answer: 'HERPES',
+                    row:18,
+                    col: 13,
+                },
+            
+            },
+            down: {
+                7: {
+                    clue: 'Inflammation du foie',
+                    answer: 'HEPATITE',
+                    row: 7,
+                    col: 8,
+                },
+            
+            11: {
+                clue: "Recherche d'une maladie",
+                answer: 'DEPISTAGE',
+                row: 13,
+                col: 22,
+            },
+            8: {
+                clue: 'La rondelle',
+                answer: 'ANAL',
+                row: 10,
+                col: 1,
+            },
+            9: {
+                clue: 'Aussi appelé “chaude-pisse”',
+                answer: 'GONORRHEE',
+                row: 11,
+                col: 14,
+            },
+            10: {
+                clue: 'Infection très contagieuse',
+                answer: 'SYPHILIS',
                 row: 16,
-                col: 13,
-              },
-              6: {
-                clue:'Virus très contagieux formant des cloques/plaies',
-                answer: 'HERPES',
-                row:18,
-                col: 13,
-              },
-         
-        },
-        down: {
-            7: {
-                clue: 'Inflammation du foie',
-                answer: 'HEPATITE',
-                row: 7,
-                col: 8,
-              },
-         
-          11: {
-            clue: "Recherche d'une maladie",
-            answer: 'DEPISTAGE',
-            row: 13,
-            col: 22,
-          },
-          8: {
-            clue: 'La rondelle',
-            answer: 'ANAL',
-            row: 10,
-            col: 1,
-          },
-          9: {
-            clue: 'Aussi appelé “chaude-pisse”',
-            answer: 'GONORRHEE',
-            row: 11,
-            col: 14,
-          },
-          10: {
-            clue: 'Infection très contagieuse',
-            answer: 'SYPHILIS',
-            row: 16,
-            col: 16,
-          },
+                col: 16,
+            },
         },
 };
 
@@ -95,7 +95,15 @@ const Crosswords ={
     padding: '10px',
     margin: '10px',
     backgroundColor: '#3ab9f0',
+}
 
+const Score = {
+    borderRadius: '5px',
+    justifyContent: 'center',
+    alignItems: 'center',
+    textAlign: 'center',
+    color: 'white',
+    backgroundColor: '#3ab9f0',
 }
 
 export default function MyPage() {
@@ -104,13 +112,18 @@ export default function MyPage() {
 
     const check = () => {
         if (crosswordRef.current.isCrosswordCorrect()) {
-            alert('Correct!');
+            setScore(++score);
+            //alert('Correct!');
         } else {
-            alert('Incorrect!');
+            shakeOn();
+            setTimeout(() => { shakeOff() ; }, 500)
+            setScore(--score);
+            //alert('Incorrect!');
         }
     };
 
     const reset = () => {
+        setScore(0);
         crosswordRef.current.reset();
     };
 
@@ -118,17 +131,29 @@ export default function MyPage() {
         crosswordRef.current.fillAllAnswers();
     }
 
-  
+    let [score, setScore] = useState(0);
+    
+    const [shake, shakeActive] = React.useState(false);
+
+    const shakeOff = () => {
+        shakeActive(false);
+    };  
+    
+    const shakeOn = () => {
+        shakeActive(true);
+    };
+
     return (
         <div style={container}>
             <div style={Crosswords}>
+            <div className={shake ? "score shake" : "score"} style={Score}><p>Score : {score}</p></div>
                 <ThemeProvider
                  theme={{
                     columnBreakpoint: '9999px',
                     gridBackground: 'none',
                     cellBackground: '#ffe',
                     cellBorder: '#fca',
-                    textColor: '#fff',
+                    textColor: '#000',
                     numberColor: '#000000',
                     focusBackground: '#f0d0aa',
                     highlightBackground: '#fa7d98',
@@ -146,8 +171,5 @@ export default function MyPage() {
             <button onClick={reset}>Reset</button>
             <button onClick={answers}>Fill all answers</button>
         </div>
-
-       
-        
     );
 }
