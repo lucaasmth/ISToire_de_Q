0623bffb127a32cea8b14a4fde20add1a5db180c
Aiveriwhair <aiveriwhair@gmail.com>
2022-12-02 03:06:45 +0100

    mini changes

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,13 +2,13 @@ import './Navbar.css';
 
 const Navbar = () => {
 
-    function toMenu(){
-        window.location.href = "#"
+    function toHome(){
+        /* Aller au home */
     }
 
     return (
       <div className="NavBar">
-            <img src="./assets/sn1.jpg" alt="" srcset="" width="100" onClick={toMenu}/>
+            <img src="./assets/sn1.jpg" alt="" srcset="" width="100" onClick={toHome}/>
             
       </div>
     );
