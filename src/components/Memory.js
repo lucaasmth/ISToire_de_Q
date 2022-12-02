47b1da78f411a1cc7ee4b93b1b957dddb10951e0
Clement Dehosse <clement.dehosse@etu.univ-grenoble-alpes.fr>
2022-12-02 04:33:48 +0100

    Memory

--- src/components/Memory.js
+++ src/components/Memory.js
@@ -15,7 +15,6 @@ const Popup = props => {
   return (
     <div className="popup-box">
       <div className="box">
-        <span className="close-icon" onClick={props.handleClose}>x</span>
         {props.content}
       </div>
     </div>
