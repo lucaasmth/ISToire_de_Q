80983644707ef53907edf810f7b7a3bbd0da0e3e
Lucas <lucas.mathieu1@etu.univ-grenoble-alpes.fr>
2022-12-02 01:03:27 +0100

    Added react-router-dom and created basic router (#4)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,47 @@
-
+import { Routes, Route, Outlet, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <p> Salut</p>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<div>Home</div>} />
+          <Route path="scenario" element={<div>Scénario</div>} />
+          <Route path="memory" element={<div>Memory</div>} />
+          <Route path="crosswords" element={<div>Crosswords</div>} />
+          <Route path="bubble" element={<div>Bubble</div>} />
+        </Route>
+      </Routes>
+    </div>
+  );
+}
+
+function Layout() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/scenario">Scenario</Link>
+          </li>
+          <li>
+            <Link to="/memory">Memory</Link>
+          </li>
+          <li>
+            <Link to="/crosswords">Crosswords</Link>
+          </li>
+          <li>
+            <Link to="/bubble">Bubble</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <hr />
+
+      <Outlet />
     </div>
   );
 }
