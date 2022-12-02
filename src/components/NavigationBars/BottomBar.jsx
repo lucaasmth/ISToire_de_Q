9b4296e52ccf33ff9d740f8a46accfd739b5c271
Guillaume Bellone <belloneg@etu.univ-grenoble-alpes.fr>
2022-12-02 04:51:26 +0100

    Fix

--- src/components/NavigationBars/BottomBar.jsx
+++ src/components/NavigationBars/BottomBar.jsx
@@ -1,102 +1,31 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import "./BottomBar.css";
-import Popup from 'reactjs-popup';
-import 'reactjs-popup/dist/index.css';
 
-//on fait une popup avec reactjs-popup au milieu de l'écran lorsque l'on clique sur l'image
 
 const BottomBar = () => {
+    const descriptions = [
+        "Cette maladie est...",
+        "2",
+        "3",
+        "4",
+        "6",
+        "7"
+    ];
 
-    const ref = useRef();
-    const openTooltip = () => ref.current.open();
-    const closeIT = () => ref.current.close();
-    const toggleTooltip = () => ref.current.toggle();
+    function onOver(index){
+        alert(descriptions[index]);
+    }
     return (
-        <div className="bottomBar__left">
-            <img src="./assets/chlamydia.jpg" alt="logo" className="bottomBar__logo" onClick={openTooltip} />
-            <Popup ref={ref} modal nested>
-                {close => (
-                    <div className="modal">
-                        <button className="close" onClick={closeIT} style={{position:'absolute',top:'0',right:'0'}}>
-                            &times;
-                        </button>
-
-                        <div className="header" style={{paddingTop:'20px'}}>  </div>
-                        
-                        <div className="actions">
-                            
-                            
-                        </div>
-                    </div>
-                )}
-            </Popup>
-            <img src="./assets/chlamydia.jpg" alt="logo" className="bottomBar__logo" onClick={toggleTooltip} />
-            <Popup ref={ref} modal nested>
-                {ich => (
-                    <div className="modal">
-                        <button className="close" onClick={closeIT} style={{position:'absolute',top:'0',right:'0'}}>
-                            &times;
-                        </button>
-
-                        <div className="header" style={{paddingTop:'20px'}}> Title </div>
-                        <div className="content">
-                            {' '}
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus, quos, voluptas? Quisquam
-                            quod, eos, ipsa, autem, nemo debitis ducimus repellendus voluptates asperiores nesciunt
-                            doloremque, expedita corporis, voluptate, voluptas, alias, minima.{' '}
-                        </div>
-                        <div className="actions">
-                            
-                            
-                        </div>
-                    </div>
-                )}
-            </Popup>
-            <img src="./assets/chlamydia.jpg" alt="logo" className="bottomBar__logo" onClick={openTooltip} />
-            <Popup ref={ref} modal nested>
-                {close => (
-                    <div className="modal">
-                        <button className="close" onClick={closeIT} style={{position:'absolute',top:'0',right:'0'}}>
-                            &times;
-                        </button>
-
-                        <div className="header" style={{paddingTop:'20px'}}> Modal Title </div>
-                        <div className="content">
-                            {' '}
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus, quos, voluptas? Quisquam
-                            quod, eos, ipsa, autem, nemo debitis ducimus repellendus voluptates asperiores nesciunt
-                            doloremque, expedita corporis, voluptate, voluptas, alias, minima.{' '}
-                        </div>
-                        <div className="actions">
-                            
-                            
-                        </div>
-                    </div>
-                )}
-            </Popup>
-            <img src="./assets/chlamydia.jpg" alt="logo" className="bottomBar__logo" onClick={openTooltip} />
-            <Popup ref={ref} modal nested>
-                {close => (
-                    <div className="modal">
-                        <button className="close" onClick={closeIT} style={{position:'absolute',top:'0',right:'0'}}>
-                            &times;
-                        </button>
-
-                        <div className="header" style={{paddingTop:'20px'}}> Modal Title </div>
-                        <div className="content">
-                            {' '}
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus, quos, voluptas? Quisquam
-                            quod, eos, ipsa, autem, nemo debitis ducimus repellendus voluptates asperiores nesciunt
-                            doloremque, expedita corporis, voluptate, voluptas, alias, minima.{' '}
-                        </div>
-                        <div className="actions">
-                            
-                            
-                        </div>
-                    </div>
-                )}
-            </Popup>
-        </div>
+        <div className='BottomBar'>
+            <div className='imContainer'>
+            <div className='im'><img alt='test' onClick={() => {onOver(0)}} src='./assets/chlamydia.jpg'></img></div>
+            <div className='im'><img onClick={() => {onOver(1)}} src='./assets/chlamydia.jpg'></img></div>
+            <div className='im'><img onClick={() => {onOver(2)}} src='./assets/chlamydia.jpg'></img></div>
+            <div className='im'><img onClick={() => {onOver(3)}} src='./assets/chlamydia.jpg'></img></div>
+            <div className='im'><img onClick={() => {onOver(4)}} src='./assets/chlamydia.jpg'></img></div>
+            <div className='im'><img onClick={() => {onOver(5)}} src='./assets/chlamydia.jpg'></img></div>
+            </div>
+      </div>
     );
 }
 export default BottomBar;
